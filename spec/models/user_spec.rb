696015a4e16f9acc58ab68b8e1cdcaf9abe23df2
Jonathan Hall <jonathan.edward.hall@gmail.com>
2020-12-30 18:54:58 -0500

    Use new User factory for some invalid tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,25 +28,25 @@ RSpec.describe User, type: :model do
   end
 
   it "is invalid without a name" do
-    user = User.new(name: nil)
+    user = FactoryBot.build(:user, name: nil)
     user.valid?
     expect(user.errors[:name]).to include("can't be blank")
   end
 
   it "is invalid without an email" do
-    user = User.new(email: nil)
+    user = FactoryBot.build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
 
   it "is invalid without a password" do
-    user = User.new(password: nil)
+    user = FactoryBot.build(:user, password: nil)
     user.valid?
     expect(user.errors[:password]).to include("can't be blank")
   end
 
   it "is invalid without a password confirmation" do
-    user = User.new(password: nil)
+    user = FactoryBot.build(:user, password_confirmation: nil)
     user.valid?
     expect(user.errors[:password_confirmation]).to include("can't be blank")
   end
