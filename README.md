1481864ea67aa5c4de628063a426ede1b7a17814
Jonathan Hall <jonathan.edward.hall@gmail.com>
2020-12-30 12:28:52 -0500

    Make two basic tests for User model.

--- README.md
+++ README.md
@@ -8,5 +8,11 @@ Run all tests.
 bundle exec rspec
 ```
 
+## Getting Started
+Homebrew
+Ruby 3.0.0
+Rails 6.1.0
+MySQL with environment variable
+
 ## Gems
 * https://github.com/rspec/rspec-rails
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
+# Be certain that the test code is doing what it’s intended to do,
+# also known as exercising the code under test. Basically, watch
+# for false positives.
 RSpec.describe User, type: :model do
+  it "is valid with a name, email, and password" do
+    User.new(
+      name: "Jonathan",
+      email: "jonathan@codeandcardboard.com",
+      password: "whatever",
+      password_confirmation: "whatever"
+    )
+    expect(User).to be_valid
+  end
+
+  it "is invalid without a name" do
+    user = User.new(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without an email"
+  it "is invalid without a password"
+  it "is invalid with a duplicate email address" do
+    # Persist the User in the test database.
+    User.create(
+      name: "Jonathan",
+      email: "jonathan@codeandcardboard.com",
+      password: "whatever",
+      password_confirmation: "whatever"
+    )
+    user = User.new(
+      name: "Alexandra",
+      email: "jonathan@codeandcardboard.com",
+      password: "whatever",
+      password_confirmation: "whatever"
+    )
+    user.valid?
+    expect(user.errors[:email].to include("has already been taken")
+  end
   pending "add some examples to (or delete) #{__FILE__}"
 end
