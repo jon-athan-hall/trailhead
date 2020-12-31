484f1baa10d0645cb29da3239d2fb2e454c006b9
Jonathan Hall <jonathan.edward.hall@gmail.com>
2020-12-31 12:22:54 -0500

    Add another user factory for admin.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,16 @@
 FactoryBot.define do
   factory :user do
-    name { 'Jonathan' }
+    name { 'tester' }
     sequence(:email) { |n| "#{name.downcase}+#{n}@test.com" }
     password { 'whatever' }
     password_confirmation { 'whatever' }
+  end
+
+  factory: :admin do
+    name { 'tester' }
+    sequence(:email) { |n| "#{name.downcase}+#{n}@test.com" }
+    password { 'whatever' }
+    password_confirmation { 'whatever' }
+    association :role
   end
 end
